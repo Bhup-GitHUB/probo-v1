873dcc83f329d8172755a4f7147e69310a869d1e
Bhupesh Kumar <bkumar_be23@thapar.edu>
2025-08-27 15:10:11 +0530

    singleton pattern

--- backend/src/services/storage.ts
+++ backend/src/services/storage.ts
@@ -1,12 +1,30 @@
 import { InrBalance, OrderbookType, StockBalancesType } from "../types/types";
 
-class StorageService{
+class StorageService {
     private static instance: StorageService;
-
     
-    public INR_BALANCES: InrBalance={};
-    public ORDERBOOK: OrderbookType={};
-    public STOCK_BALANCES: StockBalancesType={};
+    public INR_BALANCES: InrBalance = {};
+    public ORDERBOOK: OrderbookType = {};
+    public STOCK_BALANCES: StockBalancesType = {};
+  
+    private constructor() {}
+  
+    public static getInstance(): StorageService {
+      if (!StorageService.instance) {
+        StorageService.instance = new StorageService();
+      }
+      return StorageService.instance;
+    }
+  
+    public reset(): void {
+      this.INR_BALANCES = {};
+      this.ORDERBOOK = {};
+      this.STOCK_BALANCES = {};
+    }
+  }
+  
 
-    private constructor(){}
-}
+  export const storage= StorageService.getInstance();
+
+  
+
