3bd2dd5c100df5727c3a1d219cd2875337c6e1a6
Bhupesh Kumar <bkumar_be23@thapar.edu>
2025-08-27 15:01:45 +0530

    order request middleware done

--- backend/src/middlewares/middleware.ts
+++ backend/src/middlewares/middleware.ts
@@ -18,3 +18,41 @@ export const validateUserExist = async (c:Context , next:Next)=>{
     
 
 }
+
+
+export const validateOrderRequest=async (c:Context, next:Next)=>{
+    const body= await c.req.json().catch(()=> null);
+
+    if(!body){
+        return c.json<ApiResponse>({
+            success:false,
+            message:"Invalid request body"
+        }, 400)
+    }
+    const {userId, stockSymbol, quantity, price, stockType}= body;
+
+    if(!userId || !stockSymbol || !quantity || !price || !stockType){
+        return c.json<ApiResponse>({
+            success:false,
+            message:"Something is missing"
+        }, 400)
+    }
+
+    if (quantity<= 0 || price<=0){
+        return c.json<ApiResponse>({
+            success:false,
+            message:"Invalid quantity or price"
+        }, 400)
+
+        if (!["yes", "no"].includes(stockType)){
+            return c.json<ApiResponse>({
+                success:false,
+                message:"Invalid stock type"
+            }, 400)
+        }
+        
+    }
+    await next();
+
+
+}
